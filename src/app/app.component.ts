a55cacd37d79da64011f1ace3740d96f76c3aada
rakeshbhunia <rakesh.openweb@gmail.com>
2019-07-12 18:16:36 +0530

    editor implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { ViewChild } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { ToolbarService, LinkService, ImageService, HtmlEditorService, TableService } from '@syncfusion/ej2-angular-richtexteditor';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ToolbarService, LinkService, ImageService, HtmlEditorService, TableService]
 })
 export class AppComponent {
   title = 'text-editor';
+public text_user: string = "";
+public tools: object = {
+         items: [
+                'Bold', 'Italic', 'Underline', 'StrikeThrough', '|',
+                'FontName', 'FontSize', 'FontColor', 'BackgroundColor', '|',
+                'LowerCase', 'UpperCase', '|', 'Undo', 'Redo', '|',
+                'Formats', 'Alignments', '|', 'OrderedList', 'UnorderedList', '|',
+                'Indent', 'Outdent', '|', 'CreateLink','CreateTable',
+                'Image', '|', 'ClearFormat', 'Print', 'SourceCode', '|', 'FullScreen']
+        };
+
+	onSubmit(){
+		console.log("8 "+this.text_user);
+		//console.log("100" + form)
+		//console.log("200"+this.fromRTE)
+	}
 }
